68c194b6b5cca3f2df8cc32dd60f6ed940a9a784
Roland <enzinger@parkside.at>
2021-03-20 20:50:31 +0100

    Initial commit

---
+++ MyA/src/main/java/MyMainClass.java
@@ -0,0 +1,11 @@
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Another one rides the bus");
+
+	}
+
+}
---
+++ MyA/src/test/java/TestAllTheThings.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAllTheThings {
+
+	@Test
+	void testEverything() {
+		assertTrue(true);
+	}
+
+}
